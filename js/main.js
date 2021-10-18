daa4a189828089755ff243a1b1efe579c90e8193
sKadirbek <200103020@stu.sdu.edu.kz>
2021-10-19 05:34:58 +0600

    add new command for send button

--- js/main.js
+++ js/main.js
@@ -52,5 +52,21 @@ $(document).ready(function(){
         autoplaySpeed: 5000,
         dots: true,
         arrows: false,
+    });
+
+    $('a.btn_form').click( function(event){
+        event.preventDefault();
+        $('#myOverlay').fadeIn(297,	function(){
+          $('#myModal') 
+          .css('display', 'block')
+          .animate({opacity: 1}, 198);
+        });
+      });
+    
+      $('#myModal__close, #myOverlay').click( function(){
+        $('#myModal').animate({opacity: 0}, 198, function(){
+          $(this).css('display', 'none');
+          $('#myOverlay').fadeOut(297);
+        });
       });
 });
